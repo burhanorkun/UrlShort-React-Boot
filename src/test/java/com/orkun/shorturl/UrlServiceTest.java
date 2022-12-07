351e7056ce4c9d0a97fbfe12231e443b9faeab97
burhan orkun <beyazmartilar@gmail.com>
2022-12-07 14:27:47 +0300

    added base test cases

---
+++ src/test/java/com/orkun/shorturl/UrlServiceTest.java
@@ -0,0 +1,20 @@
+package com.orkun.shorturl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UrlServiceTest {
+
+
+    @Test
+    public void convertToShortUrlTest(){
+        // todo: test long to short url process
+    }
+
+    @Test
+    public void getOriginalUrlTest(){
+        // todo: test getting original url from short url
+    }
+}
--- src/test/java/com/orkun/shorturl/model/ShortUrlTest.java
+++ src/test/java/com/orkun/shorturl/model/ShortUrlTest.java
@@ -1,4 +1,35 @@
 package com.orkun.shorturl.model;
 
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class ShortUrlTest {
+
+    @Before
+    public void setUp(){
+        //todo: create shortUrl class to test
+    }
+
+    @After
+    public void tearDown(){
+        //todo: release test class
+    }
+
+    @BeforeClass
+    public static void oneTimeSetUp(){
+        // todo: create calidator or util class
+    }
+
+    @Test
+    public void testValidShortUrl(){
+        //todo: validate correct class
+    }
+
+    @Test
+    public void fullUrlCannotBeBlank(){
+        // todo: url can not be blank
+    }
 }
